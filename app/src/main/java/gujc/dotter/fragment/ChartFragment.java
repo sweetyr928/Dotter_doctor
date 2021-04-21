aba6e1d05eeedabf6f35e00322d792c23a23cbb2
sweetyr928 <39157466+sweetyr928@users.noreply.github.com>
2021-04-21 14:27:38 +0900

    차트화면 항상 맨위부터 보이게 수정

--- app/src/main/java/gujc/dotter/fragment/ChartFragment.java
+++ app/src/main/java/gujc/dotter/fragment/ChartFragment.java
@@ -2,12 +2,13 @@ package gujc.dotter.fragment;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.PointF;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.LinearSnapHelper;
+import androidx.recyclerview.widget.LinearSmoothScroller;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -15,30 +16,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
-import org.w3c.dom.Text;
-
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 
 import gujc.dotter.ChartinfoActivity;
 import gujc.dotter.R;
-import gujc.dotter.bot.BotAdapter;
 import gujc.dotter.common.FirestoreAdapter;
 import gujc.dotter.model.Board;
-import gujc.dotter.model.UserModel;
 
 public class ChartFragment extends Fragment {
     private Board chart;
@@ -74,17 +65,55 @@ public class ChartFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_chart, container, false);
         recyclerView = view.findViewById(R.id.recyclerview);
-        firestoreAdapter = new Adapter(FirebaseFirestore.getInstance()
-                .collection("Board").whereEqualTo("match", true).whereEqualTo("doctorid", myuid).orderBy("timestamp"));
-
         LinearLayoutManager manager = new LinearLayoutManager(inflater.getContext());
         manager.setOrientation(LinearLayoutManager.VERTICAL);
         manager.setReverseLayout(true);
         manager.setStackFromEnd(false);
-        recyclerView.setLayoutManager(manager); // timestamp 순으로 출력
+        recyclerView.setLayoutManager(manager);
+        firestoreAdapter = new Adapter(FirebaseFirestore.getInstance()
+                .collection("Board").whereEqualTo("match", true).whereEqualTo("doctorid", myuid).orderBy("timestamp"));
         recyclerView.setAdapter(firestoreAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManagerWithSmoothScroller(getContext()));
+        recyclerView.smoothScrollToPosition(0);
 
         return view;
+    }
+
+    public class LinearLayoutManagerWithSmoothScroller extends LinearLayoutManager {
+
+        public LinearLayoutManagerWithSmoothScroller(Context context) {
+            super(context, VERTICAL, false);
+        }
+
+        public LinearLayoutManagerWithSmoothScroller(Context context, int orientation, boolean reverseLayout) {
+            super(context, orientation, reverseLayout);
+        }
+
+        @Override
+        public void smoothScrollToPosition(RecyclerView recyclerView, RecyclerView.State state,
+                                           int position) {
+            RecyclerView.SmoothScroller smoothScroller = new TopSnappedSmoothScroller(recyclerView.getContext());
+            smoothScroller.setTargetPosition(position);
+            startSmoothScroll(smoothScroller);
+        }
+
+        private class TopSnappedSmoothScroller extends LinearSmoothScroller {
+            public TopSnappedSmoothScroller(Context context) {
+                super(context);
+
+            }
+
+            @Override
+            public PointF computeScrollVectorForPosition(int targetPosition) {
+                return LinearLayoutManagerWithSmoothScroller.this
+                        .computeScrollVectorForPosition(targetPosition);
+            }
+
+            @Override
+            protected int getVerticalSnapPreference() {
+                return SNAP_TO_START;
+            }
+        }
     }
 
     class Adapter extends FirestoreAdapter<Holder> {
